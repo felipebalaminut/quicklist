77efc464160ad611ee6354c11e0cf607592d006a
Felipe Fonseca <felipebalaminutdev@gmail.com>
2025-07-02 16:15:30 -0300

    chore: reorganize HTML structure and CSS styles

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const inputElement = document.querySelector("input[type='text']");
 const addTaskButton = document.querySelector("button.btn");
 
-//  Função "validateInput" para verificar se contém conteúdo (value  !== ""). // Return true / false
 function validateInput() {
-  if (inputElement.value.trim() !== "") {
-    return true;
-  } else {
-    return false;
+  return inputElement.value.trim() !== ""; // Vazio = false / Contéudo = true
+}
+
+// Adicionar borda vermelha caso input esteja vazio.
+function handleAddTask() {
+  const isValid = validateInput();
+
+  if (!isValid) {
+    inputElement.classList.add("error");
+  }
+
+  const itemList = document.createElement("li");
+}
+
+// Captar alguma "entrada" no input.
+function handleInputChange() {
+  const isValid = validateInput();
+
+  if (isValid) {
+    inputElement.classList.remove("error");
   }
 }
+
+// Chama a função "handleAddTask" no click do botão.
+addTaskButton.addEventListener("click", () => handleAddTask());
+
+// Chama a função "handleInputChange"
+inputElement.addEventListener("change", () => handleInputChange());
